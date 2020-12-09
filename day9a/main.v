19e660dc01613c1032a34ea0d1d364f107b689bb
Michiel Vlootman <michiel@vlootman.com>
2020-12-09 18:29:10 +0100

    cleanup

--- day9a/main.v
+++ day9a/main.v
@@ -1,8 +1,4 @@
 import os
-
-// array (queue) of 25 numbers
-// next_num to validate if sum exists in combining 2 of the 25 numbers (preamble)
-// oldest item in queue gets dequeued and next_num is added to the queue
 const (
 	preamble_len = 25
 )
@@ -13,7 +9,6 @@ fn main() {
 	numbers := input.map(it.int())
 	mut curr := preamble_len
 	preamble = numbers[..preamble_len]
-	println('preamble: $preamble')
 	for curr < numbers.len - 1 {
 		next_num := numbers[curr]
 		if !valid_number(next_num, preamble) {
@@ -21,7 +16,7 @@ fn main() {
 			return
 		}
 		// optimize later
-		preamble = preamble[1..].clone()		// clone needed to avoid bug
+		preamble = preamble[1..].clone() // clone needed to avoid bug
 		preamble << next_num
 		curr++
 	}
@@ -29,15 +24,16 @@ fn main() {
 }
 
 fn valid_number(num int, preamble []int) bool {
-	for i :=0 ; i < preamble.len; i++{
-		for j:=0; j< preamble_len; j++{
-			// skip same 
-			if i==j { continue}
-			if preamble[i] + preamble[j] == num{
+	for i := 0; i < preamble.len; i++ {
+		for j := 0; j < preamble_len; j++ {
+			// skip same
+			if i == j {
+				continue
+			}
+			if preamble[i] + preamble[j] == num {
 				return true
 			}
 		}
 	}
-	println('num:$num preamble:$preamble')
 	return false
 }
--- day9b/main.v
+++ day9b/main.v
@@ -1,47 +1,30 @@
 import os
 
-// array (queue) of 25 numbers
-// next_num to validate if sum exists in combining 2 of the 25 numbers (preamble)
-// oldest item in queue gets dequeued and next_num is added to the queue
 const (
-	answer_number = 23278925
+	answer_number = 23278925 // answer from part 1
 )
 
 fn main() {
-	
-
 	input := os.read_lines('input.txt') ?
 	numbers := input.map(it.int())
-	for i :=0 ; i < numbers.len; i++ {
-		mut values :=[numbers[i]]
-		for j :=i+1 ; j < numbers.len; j++{
+	for i := 0; i < numbers.len; i++ {
+		mut values := [numbers[i]]
+		for j := i + 1; j < numbers.len; j++ {
 			values << numbers[j]
-			values_sum := values.reduce(sum,0)
-			if  values_sum> answer_number{
+			values_sum := values.reduce(sum, 0)
+			if values_sum > answer_number {
 				break
 			}
-
-			if values_sum == answer_number{
+			if values_sum == answer_number {
 				values.sort()
-				min :=values.first() 
+				min := values.first()
 				max := values.last()
-				println('found solution: ${min} ${max} answer:${min+max}')
+				println('found solution: $min $max answer:${min + max}')
 			}
 		}
 	}
 }
+
 fn sum(a int, b int) int {
-	return a+b
-}
-fn valid_number(num int, values []int) bool {
-	for i :=0 ; i < values.len; i++{
-		for j:=0; j< values.len; j++{
-			// skip same 
-			if i==j { continue}
-			if values[i] + values[j] == num{
-				return true
-			}
-		}
-	}
-	return false
+	return a + b
 }
