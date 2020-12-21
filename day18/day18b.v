9af938a942b02285db29c5be8b429509bb7346e6
Michiel Vlootman <michiel@vlootman.com>
2020-12-21 13:53:08 +0100

    day18b

--- day18/day18b.v
+++ day18/day18b.v
@@ -1,97 +1,108 @@
-// import os
+import os
 import regex
 
-enum Ops {
-	plus
-	multiply
-}
+const (
+	file_input = './input.txt'
+)
 
 fn main() {
-	// lines := [
-	// 	// '1 + (2 * 3) + (4 * (5 + 6))', //  still becomes 51.
-	// 	// '2 * 3 + (4 * 5)'//  becomes 46.
-	// 	// '5 + (8 * 3 + 9 + 3 * 4 * 3)', //  becomes 1445.
-	// 	// '5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))',// becomes 669060.
-	// 	// '((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2', //becomes 23340
-	// ]
-	// query := r'(?P<first>\d+) \+ (?P<second>\d+)'
-	line := [
-		'1 + (2 + 1)',
-		/* 'er was eens', */ /* '(12 + 34 + (4 + 5))', *//* '(12 + 34 + (4 + 5) + (6 + 9))', *//* '(12 + 34 + (4 + 5) + 15)', *//* '1 * 2 * 3 + (4 - 1) + 12 - (3 + 1)', */
-		'((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2' /* becomes 23340 */,
-	]
-	for l in line {
-		println('\norg:$l')
-		mut expr := l
-		for {
-			mut matches := unnest(expr)
-			// no more nestings
-			if matches.len == 0 {
-				break
-			}
-			for m in matches {
-				println('\t sub:$m')
-				sub_res := sub_calc(m, false)
-				println('calc => $sub_res')
-				expr = expr.replace(m, sub_res.str())
-				println('\t after: $expr')
-			}
-		}
-
-		// now handle the additions first
-		
-		println('pre => expr:$expr')
-		sub_res := sub_calc(expr, false)
-		println('FINAL => $sub_res expr:$expr')
-	}
-	// println('unnest:${unnest('er was eens')}')
-	// println('unnest:${unnest('')
-}
-
-fn sub_calc(expression string, recurse bool) u64 {
-	mut expr := expression
-	expr = expr.replace('(', '').replace(')', '')
-	println('expr=$expr')
+	// println(calc_expression('((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2')) // 1 + (1 + 2 + 3 + (2 + 4 + 6))'))
+	lines := get_input()
 	mut res := u64(0)
-	tokens := expr.split(' ')
-	mut left_op := u64(0)
-	mut right_op := u64(0)
-	mut op := Ops.plus
-	for token in tokens {
-		match token {
-			'+' {
-				op = Ops.plus
-			}
-			'*' {
-				op = Ops.multiply
-			}
-			else {
-				right_op = token.u64()
-				match op {
-					.plus { res = left_op + right_op }
-					.multiply { res = left_op * right_op }
-				}
-			}
+	for l in lines {
+		res += calc_expression(l)
+	}
+	println(res)
+}
+
+fn get_input() []string {
+	lines := os.read_lines(file_input) or { panic(err) }
+	return lines
+}
+
+fn calc_expression(expression string) u64 {
+	// order of operations:
+	// 1. parenthesis (unnest)
+	// 2. addition
+	// 3. multiplication
+	mut res := u64(0)
+	mut expr := expression
+	mut sub_expr := ''
+	mut start := 0
+	mut end := 0
+	for {
+		sub_expr, start, end = unnest(expr)
+		sub_expr = simplify_additions(sub_expr)
+		sub_expr = simplify_multiplication(sub_expr)
+		if start == -1 {
+			return sub_expr.u64()
+		} else {
+			expr = expr[0..start] + sub_expr + expr[end..]
 		}
-		left_op = res
-		// println('token:$token res:$res left_op:$left_op right_op:$right_op')
 	}
 	return res
 }
 
-fn unnest(text string) []string {
-	match_inside_parens_query := r'(\([^()]+\))' // starts with (escaped) parenthesis open, anything but '(' or ')' multiple times, and ends on ')'. Capture group around []+ part
-	// text := '(12 + 34 + 31)' // => (4+5)
-	mut re := regex.regex_opt(match_inside_parens_query) or { panic(err) }
-	re.group_csave_flag = true
-	// println(re.get_query())
-	// #0(c#1(pa)+z ?)+  // #0 and #1 are the ids of the groups, are shown if re.debug is 1 or 2
-	idxs := re.find_all(text)
-	// println(idxs)
-	// println(re.get_group_list())
-	mut res := []string{}
-	for i := 0; i < idxs.len; i = i + 2 {
-		res << text[idxs[i]..idxs[i + 1]]
+// removes all `a + b` in expression and replaces it with the value of a+b
+// eg. 1 + 2 * 5 + 3  => 3 * 8
+fn simplify_additions(expression string) string {
+	// find pattern: (a) + (b) 
+	// convert group a and b to ints() and add them together
+	// replace the sum in expression match_a_start..match_b_end
+	mut expr := expression
+	pattern := r'(?P<a>\d+) \+ (?P<b>\d+)'
+	mut re := regex.regex_opt(pattern) or { panic(err) }
+	// text := '1 + 2 + 3' // => num_a:1 num_:2
+	for {
+		// println('expr=$expr')
+		start, end := re.find(expr)
+		// println('$start,$end')
+		if start == -1 {
+			break
+		}
+		groups := re.get_group_list()
+		a := expr[groups[0].start..groups[0].end].u64()
+		b := expr[groups[1].start..groups[1].end].u64()
+		sum := a + b
+		expr = expr[0..start] + sum.str() + expr[end..]
 	}
-	return res
+	return expr
+}
+
+// removes all `a * b` in expression and replaces it with the value of a*b
+// eg. 1 + 2 * 5 + 3  => 1 + 10 + 3
+fn simplify_multiplication(expression string) string {
+	// find pattern: (a) * (b) 
+	// convert group a and b to ints() and multiply them together
+	// replace the product in expression match_a_start..match_b_end
+	mut expr := expression
+	pattern := r'(?P<a>\d+) \* (?P<b>\d+)'
+	mut re := regex.regex_opt(pattern) or { panic(err) }
+	// text := '1 * 2 * 3' // => num_a:1 num_:2
+	for {
+		// println('expr=$expr')
+		start, end := re.find(expr)
+		// println('$start,$end')
+		if start == -1 {
+			break
+		}
+		groups := re.get_group_list()
+		a := expr[groups[0].start..groups[0].end].u64()
+		b := expr[groups[1].start..groups[1].end].u64()
+		product := a * b
+		expr = expr[0..start] + product.str() + expr[end..]
+	}
+	return expr
+}
+
+fn unnest(text string) (string, int, int) {
+	match_inside_parens_query := r'\([^()]+\)' // starts with (escaped) parenthesis open, anything but '(' or ')' multiple times, and ends on ')'. Capture group around []+ part
+	mut re := regex.regex_opt(match_inside_parens_query) or { panic(err) }
+	idxs := re.find_all(text)
+	if idxs.len == 0 {
+		return text, -1, -1
+	}
+	start := idxs[0] + 1
+	end := idxs[1] - 1
+	return text[start..end], idxs[0], idxs[1] // remove out parens
 }
